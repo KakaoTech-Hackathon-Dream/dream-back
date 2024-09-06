fd98412ee42f6fe4cf0300db9c493c887e4c6750
alswp006 <alswp006@github.com>
2024-09-06 19:51:41 +0900

    Initial commit without dream-back

---
+++ src/main/java/com/example/dream_back/DreamBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.dream_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dream_back/DreamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dream_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
